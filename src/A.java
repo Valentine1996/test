89e479b74734047b752eb85c765d2aee04af09a7
valentyn <valentyn@virtuoso.com>
2015-01-16 13:56:58 +0200

    Create class A

---
+++ src/A.java
@@ -0,0 +1,9 @@
+/**
+ * Created by valentyn on 1/16/15.
+ */
+import java.lang.*;
+public class A {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
