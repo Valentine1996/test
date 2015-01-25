758c99b0c2194f3368860322bdd008296552adb2
valentyn <valentyn@virtuoso.com>
2015-01-25 15:06:09 +0200

    Create mapper for address

--- src/A.java
+++ src/A.java
@@ -2,9 +2,19 @@
  * Created by valentyn on 1/16/15.
  */
 import java.lang.*;
+import org.apache.log4j.Logger;
 public class A {
+    static Logger log = Logger.getLogger( A.class );
+    public void message() {
+        log.debug("For Debug");
+        log.info("Deep information");
+        log.fatal("Fuck");
+        log.warn("dung");
+    }
     public static void main(String[] args){
+        
         System.out.println("Hello world");
-        int a = 5;
+        A a = new A();
+        a.message();
     }
 }
